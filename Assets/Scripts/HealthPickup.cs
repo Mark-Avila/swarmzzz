b5e39b606c3c7eec55ec1e5eaf84ab6f20856cf7
Mark Avila <avilamark96@gmail.com>
2023-06-15 23:57:30 +0800

    fixed health pickup not working due to undefined player health instance

--- Assets/Scripts/HealthPickup.cs
+++ Assets/Scripts/HealthPickup.cs
@@ -8,6 +8,11 @@ public class HealthPickup : MonoBehaviour
     [SerializeField] private AudioClip pickUpAudio;
     [SerializeField] private int healPower = 3;
 
+    private void Start()
+    {
+        playerHealth = GameObject.FindWithTag("Player").GetComponentInChildren<PlayerHealth>();    
+    }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
